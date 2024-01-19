9862590d7e5bb436b320d60d40d0e63df64917a6
jasper geerse <113439@emmauscollege.nl>
2024-01-19 21:38:29 +0000

    asefasdf

--- script.js
+++ script.js
@@ -19,12 +19,19 @@ const SPELEN = 1;
 const GAMEOVER = 2;
 var spelStatus = SPELEN;
 
-var spelerX = 600; // x-positie van speler
-var spelerY = 600; // y-positie van speler
-var spelerHealth = 200;
+var floorY = 710;
 
-var vijandX = 600;
-var vijandY = 500;
+var playerX = 600; // x-positie van speler
+var playerY = floorY - 25; // y-positie van speler
+var playerHealth = 100;
+var playerXSpeed = 2;
+var playerJumps = false;
+var jumpSpeed;
+var jumpStartSpeed =11;
+var gravity = 0.4;
+
+var enemyX = 800;
+var enemyY = 685;
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
@@ -35,18 +42,24 @@ var vijandY = 500;
  */
 var beweegAlles = function() {
   // speler
-  if (keyIsDown(65)){
-  spelerX = spelerX-2;
+  if (keyIsDown(37)){
+  playerX = playerX-playerXSpeed;
   }
-  if (keyIsDown(68)){
-    spelerX = spelerX+2;
+  if (keyIsDown(39)){
+    playerX = playerX+playerXSpeed;
   }
-  if (keyIsDown(87)){
-    spelerY = spelerY-2;
+  if(keyIsDown(67) && playerJumps === false){
+    playerJumps = true;
+    jumpSpeed = jumpStartSpeed;
   }
-  if (keyIsDown(83)){
-    spelerY = spelerY+2;
+  if(playerJumps === true){
+    playerY = playerY-jumpSpeed;
+    jumpSpeed = jumpSpeed - gravity;
   }
+  if(playerY > floorY - 25){
+    playerJumps = false;
+  }
+  
   // vijand
 
   // kogel
@@ -59,13 +72,13 @@ var beweegAlles = function() {
  */
 var verwerkBotsing = function() {
   // botsing speler tegen vijand
-  if (spelerX - vijandX<50 
-    && spelerX - vijandX>-50
-    && spelerY - vijandY<50 
-    && spelerY - vijandY>-50 
-    && spelerHealth >0) {
-    spelerHealth -= 2;
-  }
+  if (playerX - enemyX<50 
+    && playerX - enemyX>-50
+    && playerY - enemyY<50 
+    && playerY - enemyY>-50 
+    && playerHealth >0) {
+    playerHealth --;
+    }
   
   // botsing kogel tegen vijand
 
@@ -80,18 +93,21 @@ var tekenAlles = function() {
   // achtergrond
   fill("green");
   rect(0,0,1280,720);
+  // floor
+  fill("black");
+  rect(0,floorY, 1280, 10)
   // vijand
   fill("red");
-  rect(vijandX - 25, vijandY - 25, 50, 50);
+  rect(enemyX - 25, enemyY - 25, 50, 50);
   fill("black");
-  ellipse(vijandX, vijandY, 10, 10);
+  ellipse(enemyX, enemyY, 10, 10);
   // kogel
 
   // speler
   fill("wit");
-  rect(spelerX - 25, spelerY - 25, 50, 50);
+  rect(playerX - 25, playerY - 25, 50, 50);
   fill("black");
-  ellipse(spelerX, spelerY, 10, 10);
+  ellipse(playerX, playerY, 10, 10);
 
   // punten en health
   strokeWeight(5);
@@ -99,7 +115,11 @@ var tekenAlles = function() {
   rect(10,10, 210, 40);
   stroke("red");
   fill("red");
-  rect(15,15,spelerHealth,30);
+  rect(15,15,playerHealth *2,30);
+  fill("white");
+  stroke("black");
+  textSize(20);
+  text("health:"+ playerHealth, 10, 70);
   stroke("black");
   strokeWeight(1);
 };
@@ -109,9 +129,19 @@ var tekenAlles = function() {
  * anders return false
  */
 var checkGameOver = function() {
-  // check of HP 0 is , of tijd op is, of ...
+  if (playerHealth === 0 ){
+    return true;
+  }
   return false;
 };
+var gameOver = function(){
+  // teken game-over scherm
+  fill("green");
+  rect(0,0,1280,720); //achtergrond 
+  fill("white");
+  textSize(50);
+  text("game over", 100, 100);
+}
 
 /* ********************************************* */
 /* setup() en draw() functies / hoofdprogramma   */
@@ -145,7 +175,6 @@ function draw() {
     }
   }
   if (spelStatus === GAMEOVER) {
-    // teken game-over scherm
-    
+    gameOver();
   }
 }
